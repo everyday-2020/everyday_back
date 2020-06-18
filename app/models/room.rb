f36ae74e2b8b338d809efd179a4fa5c1a587889b
Jang Whemoon <palindrom615@gmail.com>
2020-06-19 04:01:08 +0900

    Validate  empty room category

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   has_many :videos
   has_and_belongs_to_many :users
-  validates :title, :complete_at, :invite_code, presence: { message: 'is empty' }
+  validates :title, :complete_at, :invite_code, :category, presence: { message: 'is empty' }
   validates :invite_code, uniqueness: { message: '%{value} is already exists' }
 end
